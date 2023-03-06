6ed2664e0a3ff9f9388111b94fd4cd620b44e105
Pwmski <69732707+Pwmski@users.noreply.github.com>
2023-03-06 20:58:30 +0000

    Update 6.406nanopolish_callmeth.sh

--- 6.406nanopolish_callmeth.sh
+++ 6.406nanopolish_callmeth.sh
@@ -13,3 +13,4 @@ nanopolish call-methylation -t 8 -r /t1-data/project/Jansenlab/pmikulsk/ont_p13m
 
 #select only chr4 (for 4p13 region) aka rows with column1 = 4
 #awk -F'\t' '{if ($1 == 4) {print} }' 406merged1_meth.tsv | awk -F'\t' '{print $1,$3,$4,$6}' > 406merged1_meth_chr4.bedgraph
+
